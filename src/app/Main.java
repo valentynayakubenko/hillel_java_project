1d50e03f1deae179ca8c54866c9d1c7e670de383
valentynay06 <valentynay@wizardsdev.com>
2025-02-02 13:24:54 +0200

    first java task

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,13 @@
 package app;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Hello World and " + name + "!");
+        scanner.close();
     }
 }
