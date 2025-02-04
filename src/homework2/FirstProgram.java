a41976a32cbcc6f44d295890cc2185b86baae249
valentynay06 <valentynay@wizardsdev.com>
2025-02-04 19:57:58 +0200

    refactore

--- src/homework2/FirstProgram.java
+++ src/homework2/FirstProgram.java
@@ -1,8 +1,8 @@
-package app;
+package homework2;
 
 import java.util.Scanner;
 
-public class Main {
+public class FirstProgram {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter your name: ");
