a07941f24d2f5ed63cd88578d872312030da2805
valentynay06 <valentynay@wizardsdev.com>
2025-02-08 17:41:50 +0200

    adjusted into separate methods

--- src/homework4/Homework4.java
+++ src/homework4/Homework4.java
@@ -3,18 +3,20 @@ package homework4;
 import java.util.Scanner;
 
 public class Homework4 {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         // 1. Обчислення квадрата числа
         System.out.print("Введіть ціле число: ");
         int number = scanner.nextInt();
-        System.out.println("Квадрат числа " + number + " дорівнює " + (number * number) + ".");
+        int squareResult = calculateSquare(number);
+        System.out.println("Квадрат числа " + number + " дорівнює " + squareResult + ".");
 
         // 2. Обчислення об'єму циліндра
         double radius = 3.5;
         double height = 10.0;
-        double volume = Math.PI * Math.pow(radius, 2) * height;
+        double volume = calculateCylinderVolume(radius, height);
         System.out.println("Об'єм циліндра з радіусом " + radius + " і висотою " + height + " дорівнює " + volume + ".");
 
         // 3. Піднесення до степеня
@@ -22,6 +24,25 @@ public class Homework4 {
         int a = scanner.nextInt();
         System.out.print("Введіть b: ");
         int b = scanner.nextInt();
-        System.out.println("Результат " + a + "^" + b + " дорівнює " +  Math.pow(a, b) + ".");
+        int powerResult = calculatePower(a, b);
+        System.out.println("Результат " + a + "^" + b + " дорівнює " + powerResult + ".");
     }
+
+    // 1. Метод обчислення квадрата числа
+    static int calculateSquare(int number) {
+        return number * number;
+    }
+
+    // 2. Метод обчислення об'єму циліндра
+    static double calculateCylinderVolume(double radius, double height) {
+        return Math.PI * Math.pow(radius, 2) * height;
+    }
+
+
+    // 3. Метод піднесення до степеня
+    static int calculatePower(int a, int b) {
+        return (int) Math.pow(a, b);
+    }
+
 }
+
